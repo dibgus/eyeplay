ed9f7f05e6371dfed6bbf788f1b0f4fdff88fd3f
Ivan <vanirocraft@gmail.com>
2017-01-12 22:14:40 -0700

    additional stuff

--- dnn-process.cpp
+++ dnn-process.cpp
@@ -1,31 +1,32 @@
 //#include <boost/python.hpp> //todo interop: send image from python, return contours
 #include <dlib/dnn.h> //dynamic neural network module with mmod of dlib
-#include <dlib/data_io> //reading the neural network definitions
-#include <dlib/gui_widgets> //gui to display results
+#include <dlib/data_io.h> //reading the neural network definitions
+#include <dlib/gui_widgets.h> //gui to display results
 #include <dlib/opencv.h> //dlib opencv bindings
-#include <cv2> //used for video stream
+#include <opencv2/opencv.hpp> //used for video stream
 
 using namespace std;
 using namespace dlib;
 using namespace cv;
 
-find_cards()
+void find_cards()
 {
+    using net_type = loss_mmod<con<1,6,6,1,1,rcon3<rcon3<rcon3<downsampler<input_rgb_image_pyramid<pyramid_down<6>>>>>>>>;
     net_type neuralnet;
-    deserialize("machine-learned/detector-dnn.dat") >> neuralnet;
+    dlib::deserialize("machine-learned/detector-dnn.dat") >> neuralnet;
     //declare and load the neural net
-    VideoCapture feed(0); //get the camera feed
+    cv::VideoCapture feed(0); //get the camera feed
     if(!feed.isOpened())
-        return -1;
-    image_window win; //where images will display. May be changed to cv's image display
+        return;
+    dlib::image_window win; //where images will display. May be changed to cv's image display
     for (;;)
     {
-        Mat frame;
+        cv::Mat frame;
         feed >> frame; //load current frame
         //cvtColor(frame, frame, BGR2RGB)
-        cv_image<bgr_pixel> cimg(frame) //convert image from mat to dlib compatible format
+        dlib::cv_image(frame) //convert image from mat to dlib compatible format
         //might need to convert to rgb, in which case, uncomment above
-        auto contours = neuralnet(frame) //detect contours with neuralnet
+        auto contours = dlib::net(frame) //detect contours with neuralnet
         win.clear_overlay(); //clear any previous bounding boxes
         win.set_image(frame); //set disp of current frame
         for (auto&& c : contours)
@@ -41,3 +42,4 @@ BOOST_PYTHON_MODULE(detectcards)
     def("find_cards", find_cards)
 }
 */
+
