a108aeb20d5b3d819bb68473e85fdf6c1bad3f8b
Ivan <vanirocraft@gmail.com>
2017-01-17 10:30:04 -0700

    More code for template matching and beginnings of template images. Code subject to change.

--- value2det.py
+++ value2det.py
@@ -2,15 +2,24 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img_rgb = cv2.imread('template-symbols/waterbottle-test.jpg')
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-template = cv2.imread('mario_coin.png',0)
+camera = cv2.VideoCapture(0)
+
+template = cv2.imread('template-symbols/Spade.jpg', 0)
 w, h = template.shape[::-1]
-
-res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-threshold = 0.8
-loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-
-cv2.imwrite('res.png',img_rgb)
+while True:
+    grabbed, img_rgb = camera.read()
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    if not grabbed:
+        print("Can't read video feed")
+        exit(1)
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    #min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    #cv2.rectangle(img_rgb, max_loc, (max_loc[0] + w, max_loc[1] + h), (0,0,255), 10)
+    threshold = 0.7
+    loc = np.where(res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
+    cv2.imshow("matched", img_rgb)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        print("Stopping Template Match...")
+        break
